6a17b98c87d4d317e8b2941f911650e69d4be606
nichro02 <nichro02@gmail.com>
2020-11-14 13:55:04 -0800

    stubbed out routes

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const db = require('./models/index.js')
 
 //body parser middleware
 app.use(express.urlencoded({ extended: false }))
-//middleware for update/delete routes
-app.use(methodOverride('_method'))
+
 
 // GET /books - list all books
-
-
+app.get('/books', (req, res)=> {
+    res.send('HITTING ALL BOOKS ROUTE')
+})
 
 // POST /books - create an entry for a new book
-
-
+app.post('/books', (req, res)=> {
+    res.send('HITTING POST BOOK ROUTE')
+})
 
 // GET /books/:id - show a specific book
-
-
+app.get('/books/:id', (req, res)=> {
+    res.send('HITTING SPECIFIC BOOK ROUTE')
+})
 
 // DELETE /books/:id - delete a specific book
+app.delete('/books/:id', (req, res) => {
+    res.send('HITTING DELETE BOOK ROUTE')
+})
+
+let server = app.listen(process.env.PORT || 8000, function() {
+    console.log(`you're listening to the smooth sounds of port ${process.env.PORT}`)
+})
+
+module.exports = server
